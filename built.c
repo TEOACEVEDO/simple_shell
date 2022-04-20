65ef287eef662f20dc39bdb97b6878a211197013
yonroa <yonanderson11@gmail.com>
2022-04-20 16:23:00 -0500

    Comments added

--- built.c
+++ built.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * searchb - Verify the Built-in
+ * @token: command to be verified
+ * @line: line input
+ * Return: 1 if is Built-in
+ * 0 if is not
+ */
 int searchb(char **token, char *line)
 {
 	int (*f)() = 0;
 
 	f = get_function(token[0]);
-        if (f != NULL)
+	if (f != NULL)
 	{
 		free(token);
 		if (f() == 1)
